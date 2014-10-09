b0e9e936aab4147ea2c5c926809d3f82d97685a8
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 18:52:24 -0400

    User association with user books established.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :user_books, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
