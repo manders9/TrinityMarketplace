dbbb1231431a2cc5ca91b7ecb043211d5f0fcae0
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-09 19:27:41 -0400

    Authentication before contributing to app.

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,4 +1,5 @@
 class UserBooksController < ApplicationController
+  # before_action :authenticate_user!, except: [:index, :show]
   def index
     @user_books = UserBook.order(created_at: :desc)
   end
