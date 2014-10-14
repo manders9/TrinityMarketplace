fc085ad9b12eb438ee4705c0333241f6b7c1364a
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-14 14:52:13 -0400

    Hound edits.

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 class ProfilePhotoUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
@@ -26,7 +25,7 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-process resize_and_pad: [100, 100, background = :transparent, gravity = :center]
+  process resize_and_pad: [100, 100, background = :transparent, gravity = :center]
   #
   # def scale(width, height)
   #   # do something
