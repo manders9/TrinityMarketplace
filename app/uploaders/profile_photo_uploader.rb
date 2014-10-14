1a343aab3d9c3583a0036f8975ac23e6cdfec168
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-14 17:12:36 -0400

    Hound edits.

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -3,7 +3,6 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
@@ -15,7 +14,6 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
-
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
