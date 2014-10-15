4737f6d646986ee37f9cb827f23d3e5e8b151b33
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-15 17:14:05 -0400

    User book factory added.

---
+++ spec/factories/user_book.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user_book do
+    title { Faker::Address.city + ' ' +  Faker::Commerce.color.capitalize }
+    author { Faker::Name.first_name + ' ' + Faker::Name.last_name }
+    condition "lightly used"
+    price 5.00
+    description { Faker::Lorem.sentence }
+    status "for sale"
+  end
+end
