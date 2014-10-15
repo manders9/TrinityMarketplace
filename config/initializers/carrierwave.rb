87af6202c04e1566114bb89b5a6427c7bcf687f8
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-15 08:21:50 -0400

    Carrierwave initializer edited.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,4 +6,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
   config.fog_directory  = ENV["S3_BUCKET"]
+  config.fog_public = false
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
