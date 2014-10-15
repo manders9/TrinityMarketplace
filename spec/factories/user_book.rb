100b52e86023a2a0cc079ff7ed96d8e99610fe68
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-15 17:44:20 -0400

    Hound edits.

--- spec/factories/user_book.rb
+++ spec/factories/user_book.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user_book do
-    title { Faker::Address.city + ' ' +  Faker::Commerce.color.capitalize }
-    author { Faker::Name.first_name + ' ' + Faker::Name.last_name }
+    title { Faker::Address.city + " " +  Faker::Commerce.color.capitalize }
+    author { Faker::Name.first_name + " " + Faker::Name.last_name }
     condition "lightly used"
     price 5.00
     description { Faker::Lorem.sentence }
