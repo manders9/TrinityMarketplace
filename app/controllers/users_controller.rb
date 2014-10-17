d9260bd196e1a251fc1b4fbe7613ffdaa0a528cd
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-10-17 13:12:58 -0400

    Users controller edited to fix omniauth.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,30 +5,30 @@ class UsersController < ApplicationController
     @user_books = @user.user_books
   end
 
-  def edit
-    # authorize! :update, @user
-  end
+  # def edit
+  #   # authorize! :update, @user
+  # end
 
-  # PATCH/PUT /users/:id.:format
-  def update
-    # authorize! :update, @user
-    # if @user.update(user_params)
-    #   sign_in(@user == current_user ? @user : current_user, bypass: true)
-    #   redirect_to @user, notice: 'Your profile was successfully updated.'
-    # else
-    #   render action: 'edit'
-    #   # format.json { render json: @user.errors, status: :unprocessable_entity }
-    # end
-  end
+  # # PATCH/PUT /users/:id.:format
+  # def update
+  #   authorize! :update, @user
+  #   if @user.update(user_params)
+  #     sign_in(@user == current_user ? @user : current_user, bypass: true)
+  #     redirect_to @user, notice: 'Your profile was successfully updated.'
+  #   else
+  #     render action: 'edit'
+  #     # format.json { render json: @user.errors, status: :unprocessable_entity }
+  #   end
+  # end
 
   # GET/PATCH /users/:id/finish_signup
   def finish_signup
     # authorize! :update, @user
     if request.patch? && params[:user] #&& params[:user][:email]
-      if @user.update(user_params)
-        @user.skip_reconfirmation!
-        sign_in(@user, bypass: true)
-        redirect_to @user, notice: 'Your profile was successfully updated.'
+      if current_user.update(user_params)
+        # current_user.skip_reconfirmation!
+        sign_in(current_user, bypass: true)
+        redirect_to current_user, notice: 'Your profile was successfully updated.'
       else
         @show_errors = true
       end
